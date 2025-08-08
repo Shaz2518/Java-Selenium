2eb1b16538ba60d4eee39ccb6fe3df3bc70ae9e4
Shaziya Shaikh <shaziyashaikh2518@outlook.com>
2025-08-08 11:04:20 +0530

    Checkbox Assignemnt

---
+++ Selenium-Java/src/DropDowns/Checkbox_Assignment.java
@@ -0,0 +1,32 @@
+package DropDowns;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Checkbox_Assignment {
+
+	public static void main(String[] args) {
+		// Invoke Browser
+		System.setProperty("webdriver.driver.chrome", "/Users/localadminuser/Documents/Drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		// Navigate to webpage
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.manage().window().maximize();
+		
+		//Number of Checkboxes
+		int chknumber = driver.findElements(By.cssSelector("input[type='checkbox']")).size();
+		System.out.println("Number of checkboxes are: " + chknumber);
+		
+		//Check firstOption 
+		System.out.println("Before Click " + driver.findElement(By.cssSelector("input[id='checkBoxOption1']")).isSelected());
+		driver.findElement(By.cssSelector("input[id='checkBoxOption1']")).click();
+		System.out.println("After Click " + driver.findElement(By.cssSelector("input[id='checkBoxOption1']")).isSelected());
+
+		//Uncheck the Selected Option
+		driver.findElement(By.cssSelector("input[id='checkBoxOption1']")).click();
+		System.out.println("After Uncheck " + driver.findElement(By.cssSelector("input[id='checkBoxOption1']")).isSelected());
+	}
+
+}
