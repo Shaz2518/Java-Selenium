221c28d9b24c65f83351f5709563d708a2742e61
Shaziya Shaikh <shaziyashaikh2518@outlook.com>
2025-08-10 00:25:00 +0530

    E2E_ECommerce

--- Selenium-Java/src/End_To_End_Project/E2E_ECommerce_AddToCart.java
+++ Selenium-Java/src/End_To_End_Project/E2E_ECommerce_AddToCart.java
@@ -1,5 +1,6 @@
 package End_To_End_Project;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class E2E_ECommerce_AddToCart {
 
@@ -14,6 +16,7 @@ public class E2E_ECommerce_AddToCart {
 		// Invoke Browser
 		System.setProperty("webdriver.driver.chrome", "/Users/localadminuser/Documents/Drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
 		// Get Product Array
 		String[] listItem = { "Cucumber", "Carrot", "Mushroom", "Banana", "Mango", "Raspberry", "Almonds", "Walnuts" };
@@ -22,8 +25,29 @@ public class E2E_ECommerce_AddToCart {
 		Thread.sleep(3000);
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
 		driver.manage().window().maximize();
+		addItem(driver,listItem);
 
 		// Store the array into ArrayList
+		
+		
+		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+		driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector("button.promoBtn")).click();
+		System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
+		driver.findElement(By.xpath("//button[text()='Place Order']")).click();
+		
+		WebElement element = driver.findElement(By.xpath("//select"));
+		Select list = new Select(element);
+		
+		element.findElement(By.xpath("//option[@value='India']")).click();
+		driver.findElement(By.className("chkAgree")).click();
+		driver.findElement(By.xpath("//button[text()='Proceed']")).click();
+
+	}
+	
+	public static void addItem(WebDriver driver, String[] listItem)
+	{
 		int j = 0;
 		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));  //Stores all the items in List
 		for (int i = 0; i < products.size(); i++) {											 // Iterate through all elements
@@ -38,10 +62,6 @@ public class E2E_ECommerce_AddToCart {
 					break;
 			}
 		}
-		
-		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
-		driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
-
 	}
 
 }
