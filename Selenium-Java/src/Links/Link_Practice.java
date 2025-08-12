ba4548b112546e38da7315a12ec7423898168e19
Shaziya Shaikh <shaziyashaikh2518@outlook.com>
2025-08-12 21:59:42 +0530

    Link Practice

--- Selenium-Java/src/Links/Link_Practice.java
+++ Selenium-Java/src/Links/Link_Practice.java
@@ -1,13 +1,19 @@
 package Links;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
 public class Link_Practice {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// Invoke Browser
 		System.setProperty("webdriver.driver.chrome", "/Users/localadminuser/Documents/Drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -29,6 +35,25 @@ public class Link_Practice {
 		WebElement firstColDriver = driver.findElement(By.xpath("//div[@id='gf-BIG']/table/tbody/tr/td[1]/ul"));
 		int firstCol = firstColDriver.findElements(By.tagName("a")).size();
 		System.out.println("First Column Link: " + firstCol);
+		
+		//Open all links in new tab and get the title of Window
+		for(int i=1; i<firstCol; i++)
+		{
+			String linkClick = Keys.chord(Keys.CONTROL,Keys.ENTER);
+			firstColDriver.findElements(By.tagName("a")).get(i).sendKeys(linkClick);
+		}
+		Thread.sleep(5000);
+		
+		//Get Titles of the Webpage
+		System.out.println("The Title of Each Tab: ");
+		Set<String> windowsHandle = driver.getWindowHandles();
+		Iterator<String> nextWindow = windowsHandle.iterator(); 
+		
+		while(nextWindow.hasNext())
+			{
+				driver.switchTo().window(nextWindow.next());
+				System.out.println(driver.getTitle());
+		}
 		
 
 	}
