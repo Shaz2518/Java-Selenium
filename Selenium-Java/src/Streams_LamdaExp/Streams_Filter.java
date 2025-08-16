75a227f4ad313850777ba88134f29c704af7a15f
Shaziya Shaikh <shaziyashaikh2518@outlook.com>
2025-08-16 21:31:22 +0530

    Filter Exp

--- Selenium-Java/src/Streams_LamdaExp/Streams_Filter.java
+++ Selenium-Java/src/Streams_LamdaExp/Streams_Filter.java
@@ -2,7 +2,7 @@ package Streams_LamdaExp;
 
 import java.util.ArrayList;
 
-public class Streams_Demo {
+public class Streams_Filter {
 
 	public static void main(String[] args) {
 		
@@ -26,9 +26,19 @@ public class Streams_Demo {
 		}
 		System.out.println("Brute Force count: " + count);
 		
-		//Lamda exp and streams
+		//Streams and Lamda exp 
 		long countLamda = name.stream().filter(s->s.startsWith("A")).count();
 		System.out.println("Stream and Lambda: " + countLamda);
+		
+		//Names with lenght >4
+		name.stream().filter(s->s.length()>4).forEach(s->System.out.println(s));
+		
+		//Only first name
+		name.stream().filter(s->s.length()>4).limit(1).forEach(s->System.out.println(s));
+		
+		//Print name length less the 4
+		name.stream().filter(s->s.length()<=4).forEach(s->System.out.println(s));
+		
 		
 		
 
