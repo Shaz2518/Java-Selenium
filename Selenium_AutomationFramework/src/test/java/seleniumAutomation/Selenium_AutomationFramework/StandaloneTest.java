07e7b1ce72db05fea97609d9cd1354b3d7e2d9c2
Shaz2518 <shaziyashaikh2518@outlook.com>
2025-09-20 23:09:11 +0530

    Selenium Framework

---
+++ Selenium_AutomationFramework/src/test/java/seleniumAutomation/Selenium_AutomationFramework/StandaloneTest.java
@@ -0,0 +1,22 @@
+package seleniumAutomation.Selenium_AutomationFramework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class StandaloneTest {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/client/");
+		
+		
+		
+
+	}
+
+}
