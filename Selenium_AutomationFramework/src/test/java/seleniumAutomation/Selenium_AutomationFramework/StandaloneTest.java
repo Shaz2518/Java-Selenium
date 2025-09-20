ed822bdb04169a695b6bea9ee3d9eb1cac19404d
Shaz2518 <shaziyashaikh2518@outlook.com>
2025-09-20 23:54:36 +0530

    Selenium Framework

--- Selenium_AutomationFramework/src/test/java/seleniumAutomation/Selenium_AutomationFramework/StandaloneTest.java
+++ Selenium_AutomationFramework/src/test/java/seleniumAutomation/Selenium_AutomationFramework/StandaloneTest.java
@@ -1,7 +1,11 @@
 package seleniumAutomation.Selenium_AutomationFramework;
 
+import java.time.Duration;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -13,12 +17,22 @@ public class StandaloneTest {
 		WebDriver driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/client/");
 		
-		//Add username password
+		//Add username password and Login
 		driver.findElement(By.id("userEmail")).sendKeys("shaikh.shaziya@gmail.com");
 		driver.findElement(By.id("userPassword")).sendKeys("Shaz@2518");
 		driver.findElement(By.id("login")).click();
+		
+		//Get Product List
+		List<WebElement> productList = driver.findElements(By.cssSelector(".mb-3"));
+		
+		WebElement prod = productList.stream().filter(product -> 
+		product.findElement(By.cssSelector("b")).getText().equals("ZARA COAT 3")).findFirst().orElse(null);
+		
+		prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+		
 		
 		
 		
